19e6e026305a49d7227c2a32b8c705ccafe6f29c
bharat <bharatbushan5320@gmail.com>
2025-11-17 11:03:40 -0800

    Add files via upload

---
+++ planner.py
@@ -0,0 +1,24 @@
+import pickle
+import pandas as pd
+import streamlit as st
+
+with open('study_planner_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+st.title("Personalised Study Planner")
+subject = st.selectbox('Subject', ['Math', 'Science', 'English', 'History', 'Programming'])
+difficulty = st.slider('Difficulty', min_value = 1, max_value = 5, value = 3)
+performance = st.slider('Performance Score', min_value = 1, max_value = 100, value = 40)
+days = st.slider("Days Left", min_value = 1, max_value = 100, value = 15)
+priority = st.selectbox('Priority Level', ['Low', 'Medium', 'High'])
+
+input_df = pd.DataFrame({
+    'Subject' : [subject],
+    'Difficulty' : [difficulty],
+    'Performance_Score' : [performance],
+    'Days_Left' : [days],
+    'Priority_Level' : [priority]
+})
+
+prediction = model.predict(input_df)
+st.success(f'You are recommeded to study {prediction[0]:.2f} hours in a day')
